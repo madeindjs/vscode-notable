75e1e456bc4ef8f3af67e14195b21990666c6ab4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-02-13 08:23:27 +0100

    factorize

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,142 @@ import matter = require("gray-matter");
 import {
   commands,
   ExtensionContext,
+  Position,
+  QuickPickItem,
   TextDocumentWillSaveEvent,
+  Uri,
   window,
   workspace,
+  WorkspaceEdit,
 } from "vscode";
 import {
-  addTagNote,
-  createNote,
-  deleteNote,
-  searchNote,
+  getMarkdownFiles,
+  isMarkdownFileMatchContent,
+  isMarkdownFileMatchTag,
+  markdownFileToQuickPickItem,
+  parseQuery,
+  uniq,
   updateFrontMatter,
-} from "./notesActions";
+} from "./utils";
+
+function createNote() {
+  const defaultContent = `---
+title: Undefined
+tags: []
+created: '${new Date().toISOString()}'
+modified: '${new Date().toISOString()}'
+---
+
+# Undefined
+
+`;
+  const newFile = Uri.parse(`untitled:untitled.md`);
+  workspace.openTextDocument(newFile).then((document) => {
+    const edit = new WorkspaceEdit();
+    edit.insert(newFile, new Position(0, 0), defaultContent);
+    return workspace.applyEdit(edit).then((success) => {
+      if (success) {
+        window.showTextDocument(document);
+      } else {
+        window.showInformationMessage("Error!");
+      }
+    });
+  });
+}
+
+async function searchNote() {
+  const queryStr = await window.showInputBox({
+    prompt: "What do you search",
+    placeHolder: "#tag content",
+  });
+
+  if (queryStr === undefined) {
+    return;
+  }
+
+  const query = parseQuery(queryStr);
+
+  const markdownFiles = await getMarkdownFiles({ showProgress: true });
+
+  const matchingMarkdownFiles = markdownFiles
+    .filter((document) => isMarkdownFileMatchTag(document, query.tags))
+    .filter((document) => isMarkdownFileMatchContent(document, query.content));
+
+  if (matchingMarkdownFiles.length === 0) {
+    window.showErrorMessage("Cannot find any notes matching this query");
+    return;
+  }
+
+  const quickPickItems: QuickPickItem[] = matchingMarkdownFiles.map((doc) =>
+    markdownFileToQuickPickItem(doc)
+  );
+
+  const selected = await window.showQuickPick<QuickPickItem>(quickPickItems);
+
+  if (selected === undefined) {
+    return;
+  }
+
+  const selectedMarkdownFile = matchingMarkdownFiles.find((d) =>
+    d.path.endsWith(selected.label)
+  );
+
+  if (selectedMarkdownFile === undefined) {
+    window.showErrorMessage("Oops. Could not find matching file");
+  } else {
+    const document = await workspace.openTextDocument(
+      Uri.file(selectedMarkdownFile.path)
+    );
+    await window.showTextDocument(document);
+  }
+}
+
+async function addTagNote() {
+  const editor = window.activeTextEditor;
+
+  if (editor === undefined) {
+    window.showErrorMessage("Please open an editor");
+    return;
+  }
+  const content = editor.document.getText();
+  const { data } = matter(content);
+
+  const oldTags = data.tags ?? [];
+
+  const newTagsStr = await window.showInputBox({
+    value: oldTags.join(","),
+    prompt: "Enter one or many tags (separated by a comma)",
+  });
+
+  if (newTagsStr === undefined) {
+    window.showErrorMessage("You don't have write any tags");
+    return;
+  }
+
+  data.tags = uniq(newTagsStr.split(",").map((t) => t.trim()));
+
+  updateFrontMatter(editor, data);
+}
+
+function deleteNote() {
+  const editor = window.activeTextEditor;
+
+  if (editor === undefined) {
+    window.showErrorMessage("Please open an editor");
+    return;
+  }
+
+  const content = editor.document.getText();
+  const { data } = matter(content);
+
+  if (data.deleted === true) {
+    delete data.deleted;
+  } else {
+    data.deleted = true;
+  }
+
+  updateFrontMatter(editor, data);
+}
 
 workspace.onWillSaveTextDocument(({ document }: TextDocumentWillSaveEvent) => {
   const editor = window.activeTextEditor;
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,18 @@
 import { promises } from "fs";
-import { extname, join } from "path";
-import { Progress, ProgressLocation, window, workspace } from "vscode";
+import { basename, extname, join } from "path";
+import {
+  Progress,
+  ProgressLocation,
+  QuickPickItem,
+  Range,
+  SnippetString,
+  TextEditor,
+  window,
+  workspace,
+} from "vscode";
 import matter = require("gray-matter");
+import yaml = require("yaml");
+const parse = require("markdown-to-ast").parse;
 
 const MD_EXTENSIONS = [".md", ".markdown"];
 
@@ -9,6 +20,26 @@ export interface MarkdownFile {
   path: string;
   content: string;
   matter: any;
+}
+interface SearchNoteQuery {
+  tags: string[];
+  content: string;
+}
+
+export function parseQuery(query: string): SearchNoteQuery {
+  const tags = [];
+
+  let tagsMatches = query.match(/\#\w*/g);
+
+  if (tagsMatches instanceof Array) {
+    tagsMatches.forEach((m) => query.replace(m, ""));
+
+    tags.push(...tagsMatches.map((m) => m.replace("#", "")));
+  }
+
+  tags.forEach((tag) => (query = query.replace(`#${tag}`, "")));
+
+  return { tags, content: query };
 }
 
 async function getProgress(title: string): Promise<Progress<any>> {
@@ -87,4 +118,82 @@ export async function getMarkdownFiles(
 
 export function uniq<T>(array: T[]): T[] {
   return array.filter((v, i, a) => a.indexOf(v) === i);
+}
+
+export function isMarkdownFileMatchTag(
+  document: MarkdownFile,
+  queryTags: string[]
+): boolean {
+  if (queryTags.length === 0) {
+    return true;
+  }
+
+  if (document.matter.tags instanceof Array) {
+    return queryTags.every((tag) => document.matter.tags.includes(tag));
+  } else {
+    return false;
+  }
+}
+
+export function isMarkdownFileMatchContent(
+  document: MarkdownFile,
+  content: string
+): boolean {
+  return document.content.includes(content);
+}
+
+export function markdownFileToQuickPickItem(
+  document: MarkdownFile
+): QuickPickItem {
+  const tags: string[] = document.matter.tags ?? [];
+  const ast = parse(document.content);
+
+  const detail: string = ast.children
+    .filter((c: any) => c.type === "Paragraph")
+    .map((c: any) => c.raw)
+    .join(" ... ");
+
+  return {
+    label: basename(document.path),
+    description: tags.map((t) => `#${t}`).join(", "),
+    detail,
+  };
+}
+
+export function updateFrontMatter(editor: TextEditor, matterData: any): void {
+  const content = editor.document.getText();
+
+  const ast = parse(content);
+
+  const titles = ast.children.filter(
+    (c: any) => c.type === "Header" && c.depth === 1
+  );
+
+  if (titles.length > 0) {
+    const raw = titles[0].raw as string;
+    // not support other syntax than `# title`
+    if (!raw.startsWith("# ")) {
+      return;
+    }
+    matterData.title = raw.replace("# ", "");
+  }
+
+  const oldMatterNode = ast.children.filter((c: any) => c.type === "Yaml")[0];
+
+  const newMatter = `---\n${yaml.stringify(matterData)}---`;
+
+  if (oldMatterNode === undefined) {
+    const snippet = new SnippetString(`${newMatter}\n`);
+    editor.insertSnippet(snippet);
+  } else {
+    const firstMatterPosition = editor.document.positionAt(
+      oldMatterNode.range[0]
+    );
+    const secondMatterPosition = editor.document.positionAt(
+      oldMatterNode.range[1]
+    );
+    const range = new Range(firstMatterPosition, secondMatterPosition);
+
+    editor.edit((editBuilder) => editBuilder.replace(range, newMatter));
+  }
 }
